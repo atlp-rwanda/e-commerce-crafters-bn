899ad305ded73672d3385a4808fad5bbb45eb9c8
chris <iradukundachristian202@gmail.com>
2024-05-22 08:45:15 +0200

    seeder for the users and carts

--- src/database/seeders/20240521193841-seed-users.js
+++ src/database/seeders/20240521193841-seed-users.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const { v4: uuidv4 } = require('uuid');
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     const profileUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXhCG6nff2emBYOKGHb6jU2zQ4C2m0LBg4Mj-eydwZyg&s'
     await queryInterface.bulkInsert('Users', [
       {
-        userId: Sequelize.UUIDV4(),
+        userId: uuidv4(),
         name: 'User1',
         email: 'user1@email.com',
         password: 'password1',
@@ -19,7 +21,7 @@ module.exports = {
         updatedAt: new Date()
       },
       {
-        userId: Sequelize.UUIDV4(),
+        userId: uuidv4(),
         name: 'User2',
         email: 'user2@email.com',
         password: 'password1',
@@ -31,7 +33,7 @@ module.exports = {
         createdAt: new Date(),
         updatedAt: new Date()
       }, {
-        userId: Sequelize.UUIDV4(),
+        userId: uuidv4(),
         name: 'User3',
         email: 'user3@email.com',
         password: 'password1',
@@ -39,44 +41,45 @@ module.exports = {
         wishlistId: null,
         cartId: null,
         role: 'buyer',
-        profile: 'profile3',
+        profile: profileUrl,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        userId: Sequelize.UUIDV4(),
+        userId: uuidv4(),
         name: 'User4',
         email: 'user4@email.com',
         password: 'password1',
         status: 'active',
         wishlistId: null,
         cartId: null,
-        role: 'vendor',
+        role: 'buyer',
         profile: profileUrl,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       {
-        userId: Sequelize.UUIDV4(),
+        userId: uuidv4(),
         name: 'User5',
         email: 'user5@email.com',
         password: 'password1',
         status: 'active',
         wishlistId: null,
         cartId: null,
-        role: 'vendor',
+        role: 'buyer',
         profile: profileUrl,
         createdAt: new Date(),
         updatedAt: new Date()
       }, {
-        userId: Sequelize.UUIDV4(),
+
+        userId: uuidv4(),
         name: 'User6',
         email: 'user6@email.com',
         password: 'password1',
         status: 'active',
         wishlistId: null,
         cartId: null,
-        role: 'vendor',
+        role: 'buyer',
         profile: profileUrl,
         createdAt: new Date(),
         updatedAt: new Date()
