f9172d529258c278b6476a3ccfb65ff76850c50e
sevelinCA <ngabosevelin@gmail.com>
2024-05-22 13:25:55 +0200

    Creating Product function

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,44 @@
+
+import { Request, Response } from "express";
+import { saveProduct } from "../services/productService";
+import Vendor from "../database/models/vendor";
+
+export const createProduct = async(req:Request,res:Response)=>{
+    try {
+        const tokenData = (req as any).token
+        let existVendor = await Vendor.findByPk(tokenData.vendorId)
+        if(existVendor){
+
+            const {name,image,description,discount,price,quantity,category} = req.body
+            if(!name || !image || !description || !price || !quantity || !category){
+                return res.status(200).json("All Field are required")
+            }
+            const data = {
+                name,
+                image,
+                description,
+                discount: discount ? discount : 0,
+                price,
+                quantity,
+                category,
+                vendorId: tokenData.id
+            }
+            const save = await saveProduct(data) 
+            if(!save){
+                return res.status(500).json({error: "Failed to save data"})
+            }
+            return res.status(201).json({message: "Product Created"})
+        } 
+        else{
+            return res.status(500).json({error: "No vendor found"})
+        }
+
+
+
+        
+    } catch (error:any) {
+        res.status(500).json({error:error.message})
+        
+    }
+
+}
