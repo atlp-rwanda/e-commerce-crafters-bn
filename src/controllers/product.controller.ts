e82ec431a401e67a7dc354ccf267e6587c658ac3
bernice uwituze <b.uwituze@alustudent.com>
2024-05-23 08:04:39 +0200

    add update and delete controller functions

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,8 +1,8 @@
-
 import { Request, Response } from "express";
 import { saveProduct, getProductById, searchProducts} from "../services/productService";
 import Vendor from "../database/models/vendor";
 import Product from "../database/models/product";
+
 
 export const createProduct = async(req:Request,res:Response)=>{
     try {
@@ -43,39 +43,58 @@ export const createProduct = async(req:Request,res:Response)=>{
         
     }
 
-}
-
-export const readProduct = async (req: Request, res: Response) => {
-    try {
-        const productId = req.params.id;
-        const product = await Product.findByPk(productId);
-        if (!product) {
-            return res.status(404).json({ error: "Product not found" });
-        }
-        return res.status(200).json(product);
-    } catch (error: any) {
-        return res.status(500).json({ error: error.message });
-    }
 };
 
-export const searchProduct = async (req: Request, res: Response) => {
-    try {
-        const { name, category } = req.query;
-        const page = parseInt(req.query.page as string) || 1;
-        const limit = parseInt(req.query.limit as string) || 10;
+// update product
 
-        const criteria: any = {};
-        if (name) criteria.name = name;
-        if (category) criteria.category = category;
+export const updateProduct = async (req: Request, res: Response) => {
+  try {
+    const tokenData = (req as any).token;
+    const existVendor = await Vendor.findByPk(tokenData.vendorId);
 
-        const products = await searchProducts(criteria, page, limit);
-        
-        if (products.length === 0) {
-            return res.status(404).json({ error: "No products found" });
-        }
-        
-        return res.status(200).json(products);
-    } catch (error: any) {
-        return res.status(500).json({ error: error.message });
+    if (existVendor) {
+      const { productId } = req.params;
+      const updateData = req.body;
+
+      const product = await Product.findByPk(productId);
+      if (!product) {
+        res.status(404).json({ error: "Product not found" });
+        return;
+      }
+
+      await product.update(updateData);
+      res
+        .status(200)
+        .json({ message: "Product updated successfully", product });
+    } else{
+        return res.status(500).json({ error: "No vendor found" });
     }
+  } catch (error: any) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+// delete product
+
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const tokenData = (req as any).token;
+    let existVendor = await Vendor.findByPk(tokenData.vendorId);
+    if (existVendor) {
+      const { productId } = req.params;
+
+      const product = await Product.findByPk(productId);
+
+      if (!product) {
+        res.status(404).json({ error: "Product not found" });
+        return;
+      }
+      await product.destroy();
+      res.status(200).json({ message: "Product deleted successfully" });
+    } else {
+        return res.status(500).json({ error: "No vendor found" });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,14 @@
-import express from "express";
-import { readProduct, searchProduct } from "../controllers/product.controller";
 
-const router = express.Router();
+import express from "express"
+import { deleteProduct, updateProduct,readProduct, searchProduct } from "../controllers/product.controller"
+const router = express.Router()
+
+
+
 
 router.get("/product/:id", readProduct);
 router.get("/products/search", searchProduct)
+router.put('/updateProduct/:id', updateProduct);
+router.delete('/deleteProduct/:id', deleteProduct);
 
 export default router;
