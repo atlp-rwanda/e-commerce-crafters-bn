aad63ee5bcd256b0bd085f7240fd9cdf86a915a3
ismaelmurekezi <ismaelmurekezi1@gmail.com>
2024-05-23 13:41:36 +0200

    deleting vendor functionality

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -32,3 +32,8 @@ module.exports = {
     }
   }
 }
+
+
+
+
+
--- src/database/config/db.config.ts
+++ src/database/config/db.config.ts
@@ -3,14 +3,18 @@ const config = require("./config");
 import dotenv from "dotenv";
 dotenv.config();
 
-const MODE: any = process.env.MODE || "development";
+const MODE:any = process.env.MODE || 'development'
 
-const connectSequelize:Sequelize = new Sequelize(config[`${MODE}`].database,config[`${MODE}`].name,config[`${MODE}`].password,{
+
+const connectSequelize:Sequelize = new Sequelize(config[`${MODE}`].url,{
     dialect: config[`${MODE}`].dialect,
     dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true
       }
+      }
+  
+}) 
 
-})
-
-
-export default connectSequelize;
+export default connectSequelize
