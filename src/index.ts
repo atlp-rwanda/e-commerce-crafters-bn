06683c787702ba12422db763dd3e597d200032ca
Prince-Kid <mucyoprinc12@gmail.com>
2024-05-23 15:14:01 +0200

    creating services and controllers for Roles

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,20 @@ import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT;
 
-
-
-
 import userRoute from "./routes/user.route";
 import swaggerRoute from "./config/SwaggerConfig";
-
+import adminRoute from "./routes/roles.route";
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
 app.use("/", userRoute);
 app.use("/api-docs", swaggerRoute);
+app.use("/admin", adminRoute);
 
+const server = app.listen(PORT, () => {
+  console.log(`Server running on Port ${PORT}`);
+});
 
-
-
- const server = app.listen(PORT, () => {
-    console.log(`Server running on Port ${PORT}`);
-  });
-
-export { app,server };
+export { app, server };
