551fbc7dda34ae13394941f3a0f7456fefac7062
HITAYEZU Jean Pierre <hitlapidis2@gmail.com>
2024-05-23 15:16:37 +0200

    Added authentication file

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from "express";
+import bcrypt from 'bcrypt';
+import { generateToken } from "../helpers/generateToken";
+import { loginFunc} from "../services/userService";
 import User from "../database/models/user";
 import { comparePassword, deleteUserById, hashPassword, saveUser, updateUser, updateUserPassword } from "../services/userService";
 
@@ -14,21 +17,28 @@ export const Welcome = async (req: Request, res: Response) => {
   }
 };
 
-export const register = async (req: Request, res: Response) => {
 
-  const { name, email, password } = req.body
-  if (!name || !email || !password) {
-    return res.status(400).json({ message: 'Please fill all fields' })
-  }
-  const duplicate: any = await User.findOne({ where: { email: email } })
-  if (duplicate) {
-    res.status(409).json({ Message: 'Email already exists' })
-  }
+export const login = async (req: Request, res: Response) => {
   try {
-    const senddata = await saveUser(req.body);
-    res.status(201).json({ message: "User created", user: senddata });
-  } catch (error: any) {
-    res.status(500).json({ error: error.message });
+    const existUser = await loginFunc(req.body);
+    if (!existUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(req.body.password, existUser.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid credentials. Try again' });
+    }
+
+    const token = await generateToken(existUser);
+    res.cookie('token',token,{httpOnly:true})
+    return res.status(200).json({
+      message: "Login successfull",
+      token,
+      user: existUser
+    });
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to log in' });
   }
 };
 
