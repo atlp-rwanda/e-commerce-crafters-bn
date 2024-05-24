52d282b57248e2c008491d050bea649b08dc84fb
Celestin25 <celestinhakorimana25@gmail.com>
2024-05-24 15:42:04 +0200

    Integrate database connection and forget password route

--- src/index.ts
+++ src/index.ts
@@ -1,51 +1,30 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// dotenv.config();
-// const PORT = process.env.PORT;
-
-// import userRoute from "./routes/user.route";
-// import swaggerRoute from "./config/SwaggerConfig";
-
-// const app = express();
-
-// app.use(express.static("public"));
-// app.use(express.json())
-// app.use("/", userRoute);
-// app.use("/api-docs", swaggerRoute);
-
-//  const server = app.listen(PORT, () => {
-//     console.log(`Server running on Port ${PORT}`);
-//   });
-
-// export { app,server };
-import connectSequelize from "./database/config/db.config";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT;
 
 import userRoute from "./routes/user.route";
-import forgetPassword from "./routes/forget.password.router"
-import resetPassword from "./routes/reset.password.route"
+import forgetPassword from "./routes/forget.password.router";
 import swaggerRoute from "./config/SwaggerConfig";
+import connectSequelize from "./database/config/db.config";
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
 app.use("/", userRoute);
+app.use("/", forgetPassword);  
 app.use("/api-docs", swaggerRoute);
-app.use("/", forgetPassword);
-app.use("/", resetPassword);
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on Port ${PORT}`);
 });
 
-export { app };
-connectSequelize.authenticate()
+connectSequelize.authenticate() 
   .then(() => {
     console.log('connected to the database')
   }).catch((error: any) => {
     console.log(error.message)
-  })
+  });
+
+export { app, server };
