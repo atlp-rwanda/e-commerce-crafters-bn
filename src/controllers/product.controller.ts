57f57f12e22c26a3b11988e9bd0ba957f1cb0407
Philimuhire <philimuhire@gmail.com>
2024-05-24 09:35:43 +0200

    add controller to read all products

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,6 @@
 
 import { Request, Response } from "express";
-import { saveProduct, getProductById, searchProducts} from "../services/productService";
+import { saveProduct, getAllProducts, getProductById, searchProducts} from "../services/productService";
 import Vendor from "../database/models/vendor";
 import Product from "../database/models/product";
 
@@ -43,6 +43,23 @@ export const createProduct = async(req:Request,res:Response)=>{
     }
 
 }
+
+export const readAllProducts = async (req: Request, res: Response) => {
+    try {
+        const page = parseInt(req.query.page as string) || 1;
+        const limit = parseInt(req.query.limit as string) || 10;
+
+        const products = await getAllProducts(page, limit);
+        
+        if (products.length === 0) {
+            return res.status(404).json({ error: "No products found" });
+        }
+        
+        return res.status(200).json(products);
+    } catch (error: any) {
+        return res.status(500).json({ error: error.message });
+    }
+};
 
 export const readProduct = async (req: Request, res: Response) => {
     try {
