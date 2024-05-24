723ba853a71beeff2c061ef0684311cccf74c62f
Philimuhire <philimuhire@gmail.com>
2024-05-24 09:37:44 +0200

    add route to read all products

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { readProduct, searchProduct } from "../controllers/product.controller";
+import { readAllProducts, readProduct, searchProduct } from "../controllers/product.controller";
 
 const router = express.Router();
 
-router.get("/product/:id", readProduct);
+router.get("/readAllProducts", readAllProducts);
+router.get("/readProduct/:id", readProduct);
 router.get("/products/search", searchProduct)
 
 export default router;
