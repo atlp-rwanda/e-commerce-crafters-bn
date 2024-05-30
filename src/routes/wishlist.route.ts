ece71fb2787640107d90bea47cf402c83c8595fd
Sng43 <s.kabare1@alustudent.com>
2024-05-30 12:32:46 +0200

    adding products to wishlist

---
+++ src/routes/wishlist.route.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { addToWishlist } from "../controllers/wishlist.controller";
+
+const router = Router();
+
+router.post("toWishlist", addToWishlist);
+
+export default router
