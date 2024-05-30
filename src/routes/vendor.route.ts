5dad13882dc4641f85d618e588d63bef893cdf29
Philimuhire <philimuhire@gmail.com>
2024-05-30 08:29:35 +0200

    added routes protection

--- src/routes/vendor.route.ts
+++ src/routes/vendor.route.ts
@@ -2,10 +2,11 @@ import express from "express"
 const route = express.Router()
 import { deletingVendor, editVendor, registerVendor } from "../controllers/vendor.controller"
 import { viewProducts } from "../controllers/product.controller"
+import { VerifyAccessToken } from "../middleware/verfiyToken"
 
 route.post('/requestVendor', registerVendor)
-route.delete('/deleteVendor/:id', deletingVendor)
-route.get('/vendorProduct/:id', viewProducts)
-route.patch('/updateVendor/:id', editVendor)
+route.delete('/deleteVendor/:id', VerifyAccessToken, deletingVendor)
+route.get('/vendorProduct/:id', VerifyAccessToken, viewProducts)
+route.patch('/updateVendor/:id', VerifyAccessToken, editVendor)
 
 export default route
