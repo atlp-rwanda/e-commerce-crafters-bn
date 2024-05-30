3691e13917ac923f0971522d9858a26c28eb87e0
ManziPatrick <patrick.manzi@amalitech.org>
2024-05-30 13:31:52 +0200

    view and clear cart

--- src/routes/cart.route.ts
+++ src/routes/cart.route.ts
@@ -1,8 +1,10 @@
-import { Router } from "express";
-import { addToCart, updateCart } from "../controllers/cart.controller";
+import { Router } from 'express';
+import { addToCart, clearCart, deleteProductFromCart, getCart } from '../controllers/cart.controller';
 
 const router = Router();
 
-router.post("/addcart", addToCart);
-router.post("/updatecart", updateCart);
+router.post('/addcart', addToCart);
+router.get('/getcart/:userId', getCart);
+router.delete('/clearcart/:userId', clearCart);
+router.delete('/cart/:userId/product/:productId', deleteProductFromCart);
 export default router;
