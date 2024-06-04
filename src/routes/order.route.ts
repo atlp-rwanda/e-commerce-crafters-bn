2d638c480d93cb231e38481138876883ffb6a9ad
sevelinCa <142162886+sevelinCa@users.noreply.github.com>
2024-06-04 17:25:57 +0200

    Update order.route.ts

--- src/routes/order.route.ts
+++ src/routes/order.route.ts
@@ -5,7 +5,7 @@ import { getOrderStatus, updateOrderStatus } from "../controllers/orderStatus.co
 import { verifyAdmin } from "../middleware/verifyRole";
 const router = express.Router();
 
-router.put("/order/:orderId/status", VerifyAccessToken, updateOrderStatus);
+router.put("/order/:orderId/order-status", VerifyAccessToken, updateOrderStatus);
 
 router.get('/order/:orderId/status',VerifyAccessToken, getOrderStatus);
 router.put('/order/:orderId/status',VerifyAccessToken, verifyAdmin, updateOrderStatus);
