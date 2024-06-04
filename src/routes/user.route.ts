9d06dc56d4a3d1def790d536a4656e82e6490b53
Philimuhire <philimuhire@gmail.com>
2024-06-04 23:53:10 +0200

    Resolved merge conflicts between ft-authorization-routes-protection and friday-demo-31-05

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,15 +1,16 @@
 import express from "express"
 import { Welcome, deleteUser, editUser, login, register, updatePassword } from "../controllers/user.controller";
 import { addReview } from "../controllers/review.controller";
+import { VerifyAccessToken } from "../middleware/verfiyToken";
 
 const route = express.Router();
 
 route.get("/", Welcome);
 
 route.post("/register", register);
-route.patch("/updateuser/:id", editUser)
-route.patch("/updatepassword/:id", updatePassword)
-route.delete("/deleteuser/:id", deleteUser);
+route.patch("/updateuser/:id", VerifyAccessToken, editUser)
+route.patch("/updatepassword/:id", VerifyAccessToken, updatePassword)
+route.delete("/deleteuser/:id", VerifyAccessToken, deleteUser);
 route.post("/login", login);
 route.post("/addreview/:id", addReview);
 
