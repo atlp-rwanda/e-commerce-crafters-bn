ea0bff67ecb625b0f8bb0e531a88b150b84936ff
sevelinCA <ngabosevelin@gmail.com>
2024-06-06 15:21:28 +0200

    Add documentation and test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./jest.config.js'] // Assuming the setup file is called jest.setup.js
+  verbose: true,
+  forceExit: true,
 };
