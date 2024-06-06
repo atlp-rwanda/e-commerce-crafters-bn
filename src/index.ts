e46bfe40a014acd00ebd3c4b9df3d255ab5f0e80
sevelinCa <142162886+sevelinCa@users.noreply.github.com>
2024-06-06 14:04:36 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import passport from "passport";
 import cors from "cors";
 import cron from "node-cron";
 import "./config/passport";
+import http from 'http';
+import { Server as SocketIOServer } from 'socket.io';
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -19,27 +21,32 @@ import forgotPassword from "./routes/forget.password.router";
 import authRoute from "./routes/auth.router";
 import roleRoute from "./routes/roles.route";
 import checkoutRoute from "./routes/checkout.router";
-import googleAuthRoute from "./routes/googleAuth.route";
+import googleAuthRoute from "./routes/googleAuth.route";;
 import cartroute from "./routes/cart.route";
+import TwoFaRoute from "./routes/2fa.route";
 import orderRoute from "./routes/order.route";
+
 import wishlistroute from "./routes/wishlist.route";
+import subscriptionRoute from "./routes/subscription.route";
+import notificationRoute from "./routes/notifications.route";
 import { checkExpiredsProduct } from "./helpers/expiring";
 
-import subscriptionRoute from "./routes/subscription.route"
-
-import notificationRoute from "./routes/notifications.route"
 const app = express();
+const httpServer = http.createServer(app);
+const ioServer = new SocketIOServer(httpServer);
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
+
  session({
   secret: "crafters1234",
   resave: false,
   saveUninitialized: true,
   cookie: { secure: false },
  })
+
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -47,7 +54,6 @@ app.use(passport.session());
 app.use(express.static("public"));
 app.use(express.json());
 
-// app.use(express.json());
 app.use("/", userRoute);
 app.use("/", authRoute);
 app.use("/", productRoute);
@@ -57,21 +63,22 @@ app.use("/", vendorRoute);
 app.use("/", roleRoute);
 app.use("/", orderRoute);
 app.use("/", checkoutRoute);
-app.use('/', googleAuthRoute);
-app.use('/', subscriptionRoute);
-app.use('/', notificationRoute)
+app.use("/", googleAuthRoute);
+app.use("/", subscriptionRoute);
+app.use("/", notificationRoute);
 app.use("/api-docs", swaggerRoute);
 app.use("/admin", adminRoute);
 app.use("/", cartroute);
 app.use("/", wishlistroute);
+app.use("/", TwoFaRoute)
 
-cron.schedule('0 0 * * *', () => {
+cron.schedule('*/2 * * * * *', () => {
     checkExpiredsProduct();
 });
-const server = app.listen(PORT, () => {
+const server = httpServer.listen(PORT, () => {
  console.log(`Server running on Port ${PORT}`);
  checkExpiredsProduct()
 });
 
 
-export { app, server };
+export { app, server, ioServer };
