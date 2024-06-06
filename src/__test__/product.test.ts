8b2b1b88006afc9d1f5beef9ddabba48d95e4704
bernice uwituze <b.uwituze@alustudent.com>
2024-06-06 15:42:22 +0200

    add swagger docs for product update and delete, order status get and update

---
+++ src/__test__/product.test.ts
@@ -0,0 +1,24 @@
+import { updateProduct } from "../controllers/product.controller";
+import { deleteProduct } from "../controllers/product.controller";
+
+describe('updateProduct', () => {
+    test("Should return success message and object", async() => {
+        
+
+    })
+
+    it('should return 403 if permission fails')
+    it('should return 404 if product not found')
+    it('should update product and return 200 if successful')
+})
+
+describe('deleteProduct', () => {
+    test("Should return success message", async() => {
+        
+
+    })
+
+    it('should return 403 if permission fails')
+    it('should return 404 if product not found')
+    it('should delete product and return 200 if successful')
+})
