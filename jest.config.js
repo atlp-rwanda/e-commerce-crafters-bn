3225c23238ef4d019bde0f112a0f198839e6de6a
chris <iradukundachristian202@gmail.com>
2024-06-08 08:45:31 +0200

    user tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['./jest.config.js'] // Assuming the setup file is called jest.setup.js
+  setupFilesAfterEnv: ['./jest.config.js'], // Assuming the setup file is called jest.setup.js
+  verbose: true,
+  clearMocks: true,
+  forceExit: true
 };
