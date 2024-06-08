8fba9915131119bddb869108f81e8b33ba738469
sevelinCA <ngabosevelin@gmail.com>
2024-06-08 23:07:16 +0200

    resolve conflict

--- src/__test__/product.test.ts
+++ src/__test__/product.test.ts
@@ -1,33 +1,28 @@
 import request from 'supertest';
 import sinon from 'sinon';
-import { productLifecycleEmitter, PRODUCT_ADDED } from '../helpers/events';
-import { NextFunction,Request,Response } from 'express';
-import setupServer from '../helpers/createServer';
+import { productLifecycleEmitter } from '../helpers/events';
+import { NextFunction, Request, Response } from 'express';
 import { app, server } from '..';
 import Product from '../database/models/product';
+import { afterAll, beforeAll, afterEach, beforeEach, describe, it, expect, jest } from '@jest/globals';
+
 jest.setTimeout(50000);
 
-
-
-
 beforeAll(() => {
-
 });
 
 afterAll(async () => {
   await new Promise(resolve => server.close(resolve)); 
 });
 
-
 jest.mock('../middleware/verfiyToken', () => {
   return {
-    VerifyAccessToken: (req:Request, res:Response, next:NextFunction) => {
+    VerifyAccessToken: (req: Request, res: Response, next: NextFunction) => {
       (req as any).token = { userId: 'test-user' }; 
       next();
     }
   };
 });
-
 
 describe('createProduct', () => {
   let checkVendorPermissionStub: sinon.SinonStub;
@@ -41,11 +36,8 @@ describe('createProduct', () => {
   });
 
   afterEach(() => {
-    checkVendorPermissionStub.restore();
-    saveProductStub.restore();
-    productLifecycleEmitterStub.restore();
+    sinon.restore();
   });
-
 
   it('should return 500 if saving product fails', async () => {
     checkVendorPermissionStub.resolves({ allowed: true });
@@ -80,7 +72,6 @@ describe('createProduct', () => {
     expect(res.body.error).toBe('Internal server error');
   });
 });
-
 
 describe('updateProduct', () => {
   let checkVendorModifyPermissionStub: sinon.SinonStub;
@@ -186,6 +177,5 @@ describe('updateProduct', () => {
     expect(res.status).toBe(200);
     expect(res.body.message).toBe('Product updated successfully');
    
-  });
-  
+  });	
 });
