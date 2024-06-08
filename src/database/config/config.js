5954967bed4cf16bfe5eaec834b44ceae36acbf6
sevelinCA <ngabosevelin@gmail.com>
2024-06-08 22:35:41 +0200

    Resolve Confilct

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,25 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const config = {
   development: {
     url: process.env.DATABASE_DEVELOPMENT_URL,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-
-    }
+    dialect: "postgres",
   },
   test: {
     url: process.env.DATABASE_TEST_URL,
-    dialect: 'postgres'
+    dialect: "postgres",
   },
   production: {
     url: process.env.DATABASE_PRODUCTION_URL,
-    dialect: 'postgres'
-  }
+    dialect: "postgres",
+  },
 };
 
 module.exports = config;
