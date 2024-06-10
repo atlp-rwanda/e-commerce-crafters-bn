d8ab2e04a9c5ea77de97d29237cdbd640306d7bc
Philimuhire <philimuhire@gmail.com>
2024-06-10 12:43:35 +0200

    added routes protection

--- src/routes/checkout.router.ts
+++ src/routes/checkout.router.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 import { createOrder } from '../controllers/checkout.controller'
 const router = express.Router()
+import { VerifyAccessToken } from '../middleware/verfiyToken'
 
-router.post('/checkout', createOrder)
+router.post('/checkout', VerifyAccessToken, createOrder)
 
 export default router
