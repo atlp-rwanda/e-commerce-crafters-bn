72426d94a6ac228999ddd5f9e2c40463a0035892
Philimuhire <philimuhire@gmail.com>
2024-06-10 21:55:46 +0200

    solved conflicts

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -9,7 +9,6 @@ import { addFeedback, addReview } from "../controllers/review.controller";
 const route = express.Router();
 
 route.get("/", Welcome);
-
 route.post("/register", register);
 route.patch("/updateuser/:id", editUser)
 route.patch("/updatepassword/:id", updatePassword)
