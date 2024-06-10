3a26427ed4da0a3358d530f47e64c53920beca4a
MUREKEZI Ismael <110295158+Ismaelmurekezi@users.noreply.github.com>
2024-06-10 14:48:08 +0200

    Update vendor.route.ts

--- src/routes/vendor.route.ts
+++ src/routes/vendor.route.ts
@@ -2,12 +2,9 @@ import express from "express"
 const route = express.Router()
 import { deletingVendor, editVendor, registerVendor } from "../controllers/vendor.controller"
 import { viewProducts } from "../controllers/product.controller"
-<<<<<<< ft-update-order
-import { selectReview } from "../controllers/review.controller"
 import { VerifyAccessToken } from "../middleware/verfiyToken"
-=======
 import { selectFeedback, selectReview } from "../controllers/review.controller"
->>>>>>> friday-demo-31-05
+
 
 route.post('/requestVendor', registerVendor)
 route.delete('/deleteVendor/:id',VerifyAccessToken,deletingVendor)
