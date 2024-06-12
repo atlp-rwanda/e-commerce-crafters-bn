d16528711303e026503b787d4549c00710022630
frerot <ntwalifrerotjacques@gmail.com>
2024-06-12 11:01:25 +0200

    added dialectOptions on test and production

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -8,10 +8,22 @@ const config = {
   test: {
     url: process.env.DATABASE_TEST_URL,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+      }
+    }
   },
   production: {
     url: process.env.DATABASE_PRODUCTION_URL,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+      }
+    }
   },
 };
 
