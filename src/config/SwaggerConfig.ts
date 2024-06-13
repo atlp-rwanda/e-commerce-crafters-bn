b502ce524c39260d4f5425bf0bd4ce18213f8f7c
frerot <ntwalifrerotjacques@gmail.com>
2024-06-13 14:53:50 +0200

    added the host link

--- src/config/SwaggerConfig.ts
+++ src/config/SwaggerConfig.ts
@@ -6,33 +6,31 @@ import swaggerUi from "swagger-ui-express";
 const router = express.Router();
 
 const options = {
-    definition: {
-      openapi: "3.0.0",
-      info: {
-        title: "Code crafters api documentation",
-        version: "1.0.0",
-        description: "Multi vendor ecommerce api docs",
-      },
-      components: {
-        securitySchemes: {
-          bearerAuth: {
-            type: "http",
-            scheme: "bearer",
-            bearerFormat: "JWT",
-          },
-        },
-      },
-      servers: [
-        {
-          url: "http://localhost:5000",
-        },
-      ],
+ definition: {
+  openapi: "3.0.0",
+  info: {
+   title: "Code crafters api documentation",
+   version: "1.0.0",
+   description: "Multi vendor ecommerce api docs",
+  },
+  components: {
+   securitySchemes: {
+    bearerAuth: {
+     type: "https",
+     scheme: "bearer",
+     bearerFormat: "JWT",
     },
-    apis: ["./src/docs/*.yaml"],
-  };
-  const swaggerSpec = swaggerJSDoc(options)
-
-
+   },
+  },
+  servers: [
+   {
+    url: "https://e-commerce-crafters-bn.onrender.com/",
+   },
+  ],
+ },
+ apis: ["./src/docs/*.yaml"],
+};
+const swaggerSpec = swaggerJSDoc(options);
 
 router.use("/", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
